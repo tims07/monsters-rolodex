3944b0da5cb234daf4a47e289577f986e1937afc
Timothy Sanders <timsanders07@gmail.com>
2023-01-23 15:24:19 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Tim
         </p>
+        <button>Change Name</button>
         <a
           className="App-link"
           href="https://reactjs.org"
